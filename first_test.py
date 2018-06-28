85cc3e598865ff702f9c61e0a1bdeb5b9d093cbc
monhustla <rdavis23344@gmail.com>
2018-06-28 10:35:34 -0500

    Update first_test.py

--- first_test.py
+++ first_test.py
@@ -15,9 +15,19 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('strange '):
+    if message.content.startswith('Strange '):
         msg = 'Hello {0.author.mention} I have been expecting you.'.format(message)
         await client.send_message(message.channel, msg)
+        
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('Strange who is the king?'):
+        msg = 'Either {0.author.mention}, or King Joey.'.format(message)
+        await client.send_message(message.channel, msg)        
 
 @client.event
 async def on_ready():
