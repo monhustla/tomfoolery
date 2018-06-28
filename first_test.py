27f28aa3f3785bd055197b244e84a0c9e2f1fa69
monhustla <rdavis23344@gmail.com>
2018-06-28 16:24:20 -0500

    Update first_test.py

--- first_test.py
+++ first_test.py
@@ -32,7 +32,7 @@ async def on_message(message):
         return
        
        
-    if message.content.startswith('Strange '):
+    if message.content.startswith('Hi Strange'):
         msg = 'Hello {0.author.mention} I have been expecting you.'.format(message)
         await client.send_message(message.channel, msg)
         return
@@ -46,7 +46,10 @@ async def on_message(message):
         msg = 'Either {0.author.mention}, or King Joey.'.format(message)
         await client.send_message(message.channel, msg)        
         return    
-       
+    if ('him') in message.content or ('Him') in message.content:
+        msg = 'Him is a godly man.'.format(message)
+        await client.send_message(message.channel, msg)
+        return       
     if message.content.startswith('Who is king joey?'):
         print(message.author)
         msg = 'King Joey is above the one above all. A pioneer, a savant, philanthropist, but most importantly he is the interpreter of the majestic 1992 Marvel Power Ranking Cards. Some say he is the Oracle of Delphi reincarnated, only time will tell.'.format(message)
