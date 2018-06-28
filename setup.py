1257d33300bfd611601bb6c2dd416fe7e0f6261a
monhustla <rdavis23344@gmail.com>
2018-06-27 21:32:20 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,89 +1,52 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may
-#  not use this file except in compliance with the License. You may obtain
-#  a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#  License for the specific language governing permissions and limitations
-#  under the License.
-
-
-import re
-import sys
-
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
+import re, os
 
-__version__ = ''
-with open('linebot/__about__.py', 'r') as fd:
-    reg = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
-    for line in fd:
-        m = reg.match(line)
-        if m:
-            __version__ = m.group(1)
-            break
+on_rtd = os.getenv('READTHEDOCS') == 'True'
 
+requirements = []
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
 
-def _requirements():
-    with open('requirements.txt', 'r') as fd:
-        return [name.strip() for name in fd.readlines()]
+if on_rtd:
+  requirements.append('sphinxcontrib-napoleon')
 
+version = ''
+with open('discord/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
-def _requirements_test():
-    with open('requirements-test.txt', 'r') as fd:
-        return [name.strip() for name in fd.readlines()]
+if not version:
+    raise RuntimeError('version is not set')
 
+readme = ''
+with open('README.md') as f:
+    readme = f.read()
 
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
+extras_require = {
+    'voice': ['PyNaCl==1.0.1'],
+}
 
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.test_args)
-        sys.exit(errno)
-
-
-with open('README.rst', 'r') as fd:
-    long_description = fd.read()
-
-setup(
-    name="line-bot-sdk",
-    version=__version__,
-    author="RyosukeHasebe",
-    author_email="hsb.1014@gmail.com",
-    maintainer="RyosukeHasebe",
-    maintainer_email="hsb.1014@gmail.com",
-    url="https://github.com/line/line-bot-sdk-python",
-    description="LINE Messaging API SDK for Python",
-    long_description=long_description,
-    license='Apache License 2.0',
-    packages=[
-        "linebot", "linebot.models"
-    ],
-    install_requires=_requirements(),
-    tests_require=_requirements_test(),
-    cmdclass={'test': PyTest},
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "License :: OSI Approved :: Apache Software License",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Topic :: Software Development"
-    ]
+setup(name='discord.py',
+      author='Rapptz',
+      url='https://github.com/Rapptz/discord.py',
+      version=version,
+      packages=['discord', 'discord.ext.commands'],
+      license='MIT',
+      description='A python wrapper for the Discord API',
+      long_description=readme,
+      include_package_data=True,
+      install_requires=requirements,
+      extras_require=extras_require,
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+      ]
 )
