353691b7f4e2862fc26a621291eeb67aa81dd14a
monhustla <rdavis23344@gmail.com>
2018-06-27 21:11:14 -0500

    Update first_test.py

--- first_test.py
+++ first_test.py
@@ -1,5 +1,16 @@
 import discord
-
+import web
+urls = (
+'/input', 'index'
+)
+class index:
+    def GET(self):
+        i = web.input(name=None)
+        return render.index(i.name)
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
+render = web.template.render('templates/')
 TOKEN = "NDM2NjczODQzMTAxMTcxNzMy.DhXOtA.zHQKVp9UKeFrCisivE4uUhQykeQ"
 
 client = discord.Client()
